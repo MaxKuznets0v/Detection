dda8af740df0680831e8dd57df6e000e58fd837b
MaxKuznets0v <max.kuznetsov@yahoo.com>
2022-03-14 23:31:42 +0300

    upd template matching

--- Task1/app.py
+++ Task1/app.py
@@ -1,8 +1,7 @@
 import PySimpleGUI as sg
 from PIL import Image
 import io
-from Task1.template_matching import TemplateMatching
-from Task1.imageFunctions import display
+import cv2
 
 
 def app():
@@ -14,44 +13,46 @@ def app():
 
     layout = [
         [sg.Column(file_input_column, justification='center')],
-        [sg.Column([[sg.Image(key='-image-'), sg.Image(key='-template-')]], justification='center')],
-        [sg.Button('Найти одно', key='-detect-'), sg.Button('Найти все', key='-detect_all-'),
-         [sg.Column([[sg.Image(key='-result-')]], justification='center')]]]
+        [sg.Column([[sg.Image(key='-image-'), sg.Image(key='-template-'), sg.Image(key='-result-')]], justification='center')],
+        [sg.Button('Template Matching', key='-detect_tm-'), sg.Button('Viola Jones', key='-detect_vj-')]]
 
     window = sg.Window('Нахождение лиц', layout, resizable=True, finalize=True)
     while True:
         event, values = window.read()
 
-        if event == '-detect-':
+        if event == '-detect_tm-':
             source_path = values['-load_source-']
             template_path = values['-load_template-']
             if len(source_path) == 0 or len(template_path) == 0:
                 continue
-            tm = TemplateMatching(Image.open(source_path))
-            faces = tm.find_one(Image.open(template_path), 50, 60)
-            if faces is not None:
-                res = display(Image.open(source_path), [faces], False)
-            else:
-                res = Image.open(source_path)
-            bio = io.BytesIO()
+            img = cv2.imread(source_path, 1)
+            temp = cv2.imread(template_path, 0)
+            w, h = temp.shape[::-1]
+            res = cv2.matchTemplate(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), temp, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            top_left = max_loc
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 3)
+
+            res = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
             res.thumbnail((600, 600))
+            bio = io.BytesIO()
             res.save(bio, format="PNG")
-            window["-template-"].update(data=io.BytesIO().getvalue())
-            window['-image-'].update(data=io.BytesIO().getvalue())
             window['-result-'].update(data=bio.getvalue())
-        elif event == '-detect_all-':
+        elif event == '-detect_vj-':
             source_path = values['-load_source-']
-            template_path = values['-load_template-']
-            if len(source_path) == 0 or len(template_path) == 0:
+            if len(source_path) == 0:
                 continue
-            tm = TemplateMatching(Image.open(source_path))
-            faces = tm.find_all(Image.open(template_path), 50, 60)
-            res = display(Image.open(source_path), faces, False)
-            bio = io.BytesIO()
+            img = cv2.imread(source_path, 1)
+            detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            faces = detector.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+            for (x, y, w, h) in faces:
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+            res = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
             res.thumbnail((600, 600))
+            bio = io.BytesIO()
             res.save(bio, format="PNG")
-            window["-template-"].update(data=io.BytesIO().getvalue())
-            window['-image-'].update(data=io.BytesIO().getvalue())
             window['-result-'].update(data=bio.getvalue())
         elif event == '-load_source-':
             file_name = values['-load_source-']
